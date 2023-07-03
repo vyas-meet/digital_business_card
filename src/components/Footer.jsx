c1593468c15014260798bb785b65ea1f26c011f7
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-03 16:21:07 -0400

    finish styling the card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <img src="src/images/twitter-icon.png" alt="Twitter icon"  />
-      <img src="src/images/facebook-icon.png" alt="Facebook icon"  />
-      <img src="src/images/instagram-icon.png" alt="Instagram icon"  />
-      <img src="src/images/git-hub-icon.png" alt="GitHub icon"  />
+      <a href="https://twitter.com/gillkomal0310" target="_blank"><img src="src/images/twitter-icon.png" alt="Twitter icon"  /></a>
+      <a href="https://www.facebook.com/reg/" target="_blank"><img src="src/images/facebook-icon.png" alt="Facebook icon"  /></a>
+      <a href="https://www.instagram.com/" target="_blank"><img src="src/images/instagram-icon.png" alt="Instagram icon"  /></a>
+      <a href="https://github.com/komalgill0310" target="_blank"><img src="src/images/git-hub-icon.png" alt="GitHub icon"  /></a>
     </footer>
   );
 }
