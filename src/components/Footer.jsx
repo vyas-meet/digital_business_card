acce0252f355028fc20e138c61cbd91c2cef5e22
unknown <meet@debox.co.in>
2023-07-06 08:41:40 +0530

    image directory changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,18 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <a href="https://twitter.com/gillkomal0310" target="_blank"><img src="src/images/twitter-icon.png" alt="Twitter icon"  /></a>
-      <a href="https://www.facebook.com/reg/" target="_blank"><img src="src/images/facebook-icon.png" alt="Facebook icon"  /></a>
-      <a href="https://www.instagram.com/" target="_blank"><img src="src/images/instagram-icon.png" alt="Instagram icon"  /></a>
-      <a href="https://github.com/komalgill0310" target="_blank"><img src="src/images/git-hub-icon.png" alt="GitHub icon"  /></a>
+      <a href="https://twitter.com/gillkomal0310" target="_blank">
+        <img src="images/twitter-icon.png" alt="Twitter icon" />
+      </a>
+      <a href="https://www.facebook.com/reg/" target="_blank">
+        <img src="images/facebook-icon.png" alt="Facebook icon" />
+      </a>
+      <a href="https://www.instagram.com/" target="_blank">
+        <img src="images/instagram-icon.png" alt="Instagram icon" />
+      </a>
+      <a href="https://github.com/komalgill0310" target="_blank">
+        <img src="images/git-hub-icon.png" alt="GitHub icon" />
+      </a>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <img src="./src/images/business-card-photo.jpg" className="header-photo" />
+      <img src="images/business-card-photo.jpg" className="header-photo" />
     </header>
   );
 }
